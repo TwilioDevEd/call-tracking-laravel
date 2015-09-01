2ebdcad1123eb67831d0b26a16c24bc29e0beb33
Juan Pablo Santos <jsantos@stackbuilders.com>
2015-09-01 11:28:42 -0500

    Tests for available number controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,15 +11,20 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get(
+    '/', function () {
+        return redirect(route('available_number.index'));
+    }
+);
 
 Route::resource(
     'available_number', 'AvailableNumberController', ['only' => ['index']]
 );
 
 Route::resource(
-    'lead_source', 'LeadSourceController',
-    ['only' => ['index', 'store', 'edit', 'update', 'destroy']]
+    'lead_source', 'LeadSourceController', ['except' => ['create', 'show']]
+);
+
+Route::resource(
+    'lead', 'LeadController', ['only' => ['index', 'store']]
 );
