630cab5b4f61cb5f982c1cc23f430b2c21de6d0d
Eliecer Hernandez <ehernandez@stackbuilders.com>
2016-07-20 18:06:57 -0500

    Fix travis provisioning issues for testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -16,10 +16,21 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
      */
     public function createApplication()
     {
-        $app = require __DIR__.'/../bootstrap/app.php';
+        $app = require __DIR__ . '/../bootstrap/app.php';
 
         $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
         return $app;
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+        Artisan::call('migrate');
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
     }
 }
